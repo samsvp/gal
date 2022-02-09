df14fb0c472bc29390551f0a32a9b971cd1a06ab
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2022-02-09 00:06:36 -0300

    Added python script to remake genes image

---
+++ scripts/make_img.py
@@ -0,0 +1,160 @@
+# %%
+import argparse
+import numpy as np
+import skimage
+
+from typing import List, Tuple
+
+SCALE = 0.05
+
+
+def get_angles(genes: np.ndarray) -> np.ndarray:
+    angles = genes[:,3].reshape(-1)
+    return 2 * np.pi * angles - np.pi
+
+
+def get_scale(genes: np.ndarray, 
+        og_size: Tuple[int, int],
+        target_size: Tuple[int, int]) -> np.ndarray:
+    r = target_size[0] / og_size[0]
+    scales = genes[:,2].reshape(-1) * r
+    return (0.7 * scales + 0.3) * r
+
+
+def get_xy(genes: np.ndarray) -> \
+        Tuple[np.ndarray, np.ndarray]:
+    x = genes[:,0].reshape(-1)
+    y = genes[:,1].reshape(-1)
+    return (x, y)
+
+
+def load_data(filepath: str) -> \
+        Tuple[List[int], List[str], np.ndarray]:
+    """
+    Loads the gene data from file
+    """
+    with open(filepath) as f:
+        data = f.read().split("end")
+
+    dna_dims = [int(d) for d in 
+        data[0].split(":")[-1].split(" ")]
+    og_dims = [int(d) for d in 
+        data[1].split(":")[-1].split(" ")]
+    img_files = [d.replace("\t","") for d in 
+        data[2].split(":")[-1].split("\n") if d]
+    flat_genes = np.array([float(d) for d in 
+        data[3].split(":")[-1].split("\n") if d])
+    genes = flat_genes.reshape(dna_dims)
+    return (og_dims, img_files, genes)
+
+    
+def add_imgs(img1: np.ndarray, img2: np.ndarray) \
+        -> np.ndarray:
+    """
+    """
+    mask = img2[:,:,-1].astype(bool)[...,None]
+    return img1 * (1 - mask) + img2 * mask
+
+
+def resize(img: np.ndarray, s: float) -> np.ndarray:
+    """
+    Resizes image by the given scale percentage
+    """
+    size = (int(s * img.shape[0]), int(s * img.shape[1]))
+    r_img = (skimage.transform.resize(
+        img / 255, size, anti_aliasing=True) * 255).astype(int)
+    return r_img
+
+
+def rotate(img: np.ndarray, rad_angle: float) -> np.ndarray:
+    """
+    Rotates the image by the given angle
+    """
+    angle = np.rad2deg(rad_angle)
+    r = skimage.transform.rotate(img / 255, angle, 1)
+    return (r * 255).astype(int)
+
+
+
+def get_section(img:np.ndarray, _x: int, _y: int, 
+        size_x: int, size_y: int) -> np.ndarray:
+    """
+    Gets the image section contained within
+    x + size_x, y + size_y
+    """
+    x = int(_x * size_x)
+    y = int(_y * size_y)
+    return img[y:y+size_y,x:x+size_x,:]
+
+
+def create_img(genes: np.ndarray, img_files: List[str],
+        target_size: Tuple[int, int]) -> np.ndarray:
+    """
+    Creates the image using the given metadata
+    """
+    angles = get_angles(genes)
+    scales = get_scale(genes, og_dims, target_size)
+    x, y = get_xy(genes)
+
+    res_img = np.zeros(
+        (*target_size[:2], 4), dtype=int)
+
+    objs = [skimage.io.imread(img_file) 
+        for img_file in img_files]
+
+    for i in range(x.shape[0]):
+        def transform(_obj: np.ndarray, scale: float,
+                angle: float) -> np.ndarray:
+            """
+            Rotates and resizes the image    
+            """
+            obj = resize(rotate(_obj, angle),
+                scale * SCALE)
+            return obj
+
+        obj = transform(objs[i], scales[i], angles[i])
+
+        s_x = int(x[i] * res_img.shape[1])
+        s_y = int(y[i] * res_img.shape[0])
+        e_x = s_x + obj.shape[1]
+        e_y = s_y + obj.shape[0]
+
+        img = res_img[s_y:e_y,s_x:e_x,:]
+        
+        # adds the image together
+        mask = obj[:,:,-1].astype(bool)
+        try:
+            img[mask] = obj[mask]
+            res_img[s_y:e_y,s_x:e_x,:] = img
+        except:
+            print(f"Image {i} skipped due to image bounds")
+    
+    return res_img
+
+
+# %%
+if __name__ == "__main__":
+    # example usage: 
+    # python3 make_img.py --path ../build/iter_0.txt \
+    #           -r 2 -z 0.05
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", 
+        type=str, help="File to read metadata from")
+    parser.add_argument("-s", "--save_path", default="out.png",
+        type=str, help="Path to save image")
+    parser.add_argument("-r", "--resize", default=1,
+        type=float, help="Resize the original target image")
+    parser.add_argument("-z", "--scale", default=1,
+        type=float, help="Scale the value of the original metadata")
+
+    args = parser.parse_args()
+    
+    print(f"{args}")
+
+    og_dims, img_files, genes = load_data(args.path)
+    target_size = (args.resize * np.array(og_dims)).astype(int)
+    SCALE = args.scale
+    img = create_img(genes, img_files, target_size)
+    skimage.io.imsave(args.save_path, img / 255)
+
+# %%
