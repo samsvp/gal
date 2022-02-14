4a39250f25797a258bc578a39ac4ffca03bf659e
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2022-02-14 00:28:39 -0300

    Added code to transform vector of images into a pure arrayfire array

--- include/image_functions.hpp
+++ include/image_functions.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <functional>
 #include <arrayfire.h>
 
@@ -25,6 +26,12 @@ namespace ifs
      * where 'b' is the batch size
      */
     af::array translate(af::array imgs, af::array t);
+
+    /*
+     * Transforms a vector of images into an array
+     * of same sized images
+     */
+    af::array vec_to_array(std::vector<af::array> v, int channels);
 
     /*
      * Puts one image above another. A function
@@ -66,12 +73,54 @@ namespace ifs
         int b = imgs.dims(3);
         af::array T = af::identity(3, 3, 1, b);
         // yx axis translations
-        T(0, 2, af::span, af::span) = t(0, af::span);
-        T(1, 2, af::span, af::span) = t(1, af::span);
+        T(0, 2, af::span) = t(0, af::span);
+        T(1, 2, af::span) = t(1, af::span);
 
         af::array translated = af::transform(imgs, T.T(), 
             imgs.dims(0), imgs.dims(1));
         return translated;
+    }
+
+
+    af::array vec_to_array(std::vector<af::array> imgs_vec, int channels=4)
+    {
+
+        // computes maximum width and height
+        auto cmp_size = [](af::array a1, af::array a2, int d){ return a1.dims(d) < a2.dims(d); };
+
+        int max_h = (*std::max_element(imgs_vec.begin(), imgs_vec.end(), 
+            [&cmp_size](auto a1, auto a2) { return cmp_size(a1, a2, 0); })).dims(0);
+        int max_w = (*std::max_element(imgs_vec.begin(), imgs_vec.end(),
+            [&cmp_size](auto a1, auto a2) { return cmp_size(a1, a2, 1); })).dims(1);
+        
+        // creates array to hold all images
+        af::array arr = af::constant(0, max_h, max_w, channels, imgs_vec.size());
+
+        // returns the begin and end padding values needed to have the image be of max shape
+        auto get_pad_values = [](af::array img, int max, int d) {
+            int delta = max - img.dims(d);
+            int pad_begin = delta / 2;
+            int pad_end = delta % 2 ?  delta / 2 + 1 : delta / 2;
+            return std::make_pair(pad_begin, pad_end);
+        };
+
+        
+        for (int i=0; i<imgs_vec.size(); i++)
+        {
+            // get padding values
+            af::array img = imgs_vec[i];
+            std::pair<int, int> d0 = get_pad_values(img, max_h, 0);
+            std::pair<int, int> d1 = get_pad_values(img, max_w, 1);
+            
+            af::dim4 begin_padding = af::dim4(d0.first, d1.first, 0, 0);
+            af::dim4 end_padding = af::dim4(d0.second, d1.second, 0, 0);
+            
+            // pad images
+            af::array p_img = af::pad(img, begin_padding, end_padding, AF_PAD_ZERO);
+            arr(af::span, af::span, af::span, i) = p_img;
+        }
+
+        return arr;
     }
 
 
