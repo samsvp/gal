f7b01bafddc6dc6a24727e66f3e536ab21ab9f4c
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2022-02-13 23:25:13 -0300

    Added code to do batch translation on images

--- include/image_functions.hpp
+++ include/image_functions.hpp
@@ -8,9 +8,48 @@ namespace ifs
 {
     const float PI = 3.14159;
 
+    // ---------------------------------------------
+    // Definitions
+    // ---------------------------------------------
+
     /*
      * Adds two images using the alpha channel
      */
+    const af::array alpha_blend(const af::array &foreground, 
+        const af::array &background, const af::array &mask);
+
+    /*
+     * Does a batch translation on the given image
+     * 't' must be of size 2 x 1 x 1 x b
+     * where imgs is of size w x h x c x b
+     * where 'b' is the batch size
+     */
+    af::array translate(af::array imgs, af::array t);
+
+    /*
+     * Puts one image above another. A function
+     * can be passed to perform a transformation
+     * on the calculated parameters before merging
+     */
+    af::array add_imgs(af::array& foreground, 
+        af::array& background, af::array _x, 
+        af::array _y, float scale,
+        bool resize, bool rotate, float angle);
+
+    af::array add_imgs(af::array& foreground, 
+        af::array& background, af::array _x, 
+        af::array _y, float scale,
+        bool resize, bool rotate, float _angle,
+        std::function<af::array(
+            af::array&, af::array&, af::array&, af::array&, af::array&)> f,
+        bool skip_f);
+
+
+
+    // ---------------------------------------------
+    // Implementations
+    // ---------------------------------------------
+
     const af::array alpha_blend(const af::array &foreground, 
         const af::array &background, const af::array &mask)
     {
@@ -18,6 +57,21 @@ namespace ifs
         if (mask.dims(2) != foreground.dims(2))
             tiled_mask = tile(mask, 1, 1, foreground.dims(2));
         return foreground * tiled_mask + (1.0f-tiled_mask) * background;
+    }
+
+
+    af::array translate(af::array imgs, af::array t)
+    {
+        // translation matrix
+        int b = imgs.dims(3);
+        af::array T = af::identity(3, 3, 1, b);
+        // yx axis translations
+        T(0, 2, af::span, af::span) = t(0, af::span);
+        T(1, 2, af::span, af::span) = t(1, af::span);
+
+        af::array translated = af::transform(imgs, T.T(), 
+            imgs.dims(0), imgs.dims(1));
+        return translated;
     }
 
 
